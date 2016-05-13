1c4839fe471d2b99224fe0cd90d139591de00115
James Murphy <jmurphy84@googlemail.com>
2016-05-13 08:11:00 +0100

    Added isHigherThan test for a Set

--- src/main/java/com/rentalcars/dojonorth/tdd/poker/Set.java
+++ src/main/java/com/rentalcars/dojonorth/tdd/poker/Set.java
@@ -1,5 +1,19 @@
 package com.rentalcars.dojonorth.tdd.poker;
 
 public enum Set {
-    PAIR
+    PAIR(1), THREE_OF_A_KIND(2);
+
+    private final int priority;
+
+    Set(int priority) {
+        this.priority = priority;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+
+    public boolean isHigherThan(Set otherSet) {
+        return this.priority > otherSet.priority;
+    }
 }
---
+++ src/test/java/com/rentalcars/dojonorth/tdd/poker/SetTest.java
@@ -0,0 +1,18 @@
+package com.rentalcars.dojonorth.tdd.poker;
+
+import static com.rentalcars.dojonorth.tdd.poker.Set.PAIR;
+import static com.rentalcars.dojonorth.tdd.poker.Set.THREE_OF_A_KIND;
+import static org.junit.Assert.assertFalse;
+
+import org.junit.Test;
+
+/**
+ * Created by jamesmurphy on 13/05/2016.
+ */
+public class SetTest {
+
+    @Test
+    public void threeOfAKindIsHigherThanAPair() throws Exception {
+        assertFalse(PAIR.isHigherThan(THREE_OF_A_KIND));
+    }
+}
