2cb8b8f27b31ae91243e63838dac79818d76257e
James Murphy <jmurphy84@googlemail.com>
2016-05-14 10:24:49 +0100

    Evaluates best hand for PAIR and THREE_OF_A_KIND

--- src/main/java/com/rentalcars/dojonorth/tdd/poker/Hand.java
+++ src/main/java/com/rentalcars/dojonorth/tdd/poker/Hand.java
@@ -1,8 +1,11 @@
 package com.rentalcars.dojonorth.tdd.poker;
 
-import java.util.List;
+import com.sun.javafx.collections.UnmodifiableListSet;
+
+import java.util.*;
 
 import static com.rentalcars.dojonorth.tdd.poker.Set.PAIR;
+import static com.rentalcars.dojonorth.tdd.poker.Set.THREE_OF_A_KIND;
 
 /**
  * Created by jamesmurphy on 12/05/2016.
@@ -14,7 +17,45 @@ public class Hand {
         this.cards = cards;
     }
 
-    public Set evaluate() {
-        return PAIR;
+    public Set evaluateBestHand() {
+        Set set = null;
+        int noOfTypes = 0;
+        Map handMap = new HashMap<String, Integer>();
+
+        // Creates a map e.g.
+        // 2, 2
+        // 3, 1
+        // 4, 0
+        for (String card : cards) {
+            String type = card.substring(0, 1);
+            Integer currentTypeCount = (Integer) handMap.get(type);
+            if (currentTypeCount == null) {
+                currentTypeCount = 0;
+            }
+            currentTypeCount = currentTypeCount + 1;
+            handMap.put(type, currentTypeCount);
+        }
+
+        Iterator<String> handMapIter = handMap.keySet().iterator();
+        java.util.Set<Set> hands = new LinkedHashSet<Set>();
+        while (handMapIter.hasNext()) {
+            String type = handMapIter.next();
+            if ((Integer) handMap.get(type) == 2) {
+                hands.add(PAIR);
+            }
+            if ((Integer) handMap.get(type) == 3) {
+                hands.add(THREE_OF_A_KIND);
+            }
+        }
+
+        Iterator<Set> handsIter = hands.iterator();
+        Set bestHand = null;
+        while (handsIter.hasNext()) {
+            bestHand = handsIter.next();
+        }
+
+        return bestHand;
     }
 }
+
+
--- src/test/java/com/rentalcars/dojonorth/tdd/poker/PokerGameTest.java
+++ src/test/java/com/rentalcars/dojonorth/tdd/poker/PokerGameTest.java
@@ -1,19 +1,24 @@
 package com.rentalcars.dojonorth.tdd.poker;
 
-import com.rentalcars.dojonorth.tdd.poker.Hand;
 import org.junit.Test;
 
 import java.util.Arrays;
 
-import static com.rentalcars.dojonorth.tdd.poker.Set.PAIR;
+import static com.rentalcars.dojonorth.tdd.poker.Set.*;
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 
 public class PokerGameTest {
 
     @Test
-    public void handTypeIsAPair() throws Exception {
+    public void twoTwosIsAPair() throws Exception {
         Hand hand = new Hand(Arrays.asList("2H", "2D", "3H", "4H", "5S"));
-        assertThat(hand.evaluate(), is(PAIR));
+        assertThat(hand.evaluateBestHand(), is(PAIR));
+    }
+    
+    @Test
+    public void threeTwosIsThreeOfAKind() throws Exception {
+        Hand hand = new Hand(Arrays.asList("2H", "2D", "2C", "3D", "4H"));
+        assertThat(hand.evaluateBestHand(), is(THREE_OF_A_KIND));
     }
 }
