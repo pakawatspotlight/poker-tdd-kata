34a1d06e42bdae102fbc113363081daa922f649e
James Murphy <jmurphy84@googlemail.com>
2016-05-26 21:11:56 +0100

    Added javascript jasmine tests

---
+++ javascript/test/spec/pokerHandsSpec.js
@@ -0,0 +1,9 @@
+(function () {
+  'use strict';
+
+  describe('Poker hands', function () {
+      it('should evaluate hand as a pair', function () {
+        expect(true).toBe(false);
+      });
+  });
+})();
