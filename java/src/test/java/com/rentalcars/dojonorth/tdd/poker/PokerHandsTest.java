3dadf6aac57a1364da95d3667555093f77ca7f2a
James Murphy <jmurphy84@googlemail.com>
2016-05-27 07:24:44 +0100

    Added PokerHandsTest class

---
+++ java/src/test/java/com/rentalcars/dojonorth/tdd/poker/PokerHandsTest.java
@@ -0,0 +1,11 @@
+package com.rentalcars.dojonorth.tdd.poker;
+
+import org.junit.Test;
+
+public class PokerHandsTest {
+
+    @Test
+    public void aPair() throws Exception {
+        /** Write your assertion first **/
+    }
+}
