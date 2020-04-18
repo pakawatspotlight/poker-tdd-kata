950611aa9a19904ad4fb5be2288ef1bc0c8b20b7
Pakawat Anekwiroj <pakawatanekwiroj@Pakawats-MacBook-Pro.local>
2020-04-18 21:14:04 +0700

    HighCard - Add all case for only last card

--- javascript/src/pokerHandEvaluator.js
+++ javascript/src/pokerHandEvaluator.js
@@ -1,7 +1,29 @@
 class PokerHandEvaluator {
-    evaluate(playerHand) {
-        return "high card: Ace";
-    }
+  evaluate(playerHand) {
+    const cards = playerHand.split(" ");
+    const mapper = this.getCardText();
+    const highCard = mapper[cards[4][0]];
+    return "high card: " + highCard;
+  }
+
+  getCardText() {
+    return {
+      "2": "2",
+      "3": "3",
+      "4": "4",
+      "5": "5",
+      "6": "6",
+      "7": "7",
+      "8": "8",
+      "9": "9",
+      "9": "9",
+      "10": "10",
+      J: "Jack",
+      Q: "Queen",
+      K: "King",
+      A: "Ace",
+    };
+  }
 }
 
 exports.PokerHandEvaluator = PokerHandEvaluator;
--- javascript/test/pokerHandEvaluator.spec.js
+++ javascript/test/pokerHandEvaluator.spec.js
@@ -1,12 +1,32 @@
-const {PokerHandEvaluator} = require('../src/pokerHandEvaluator');
+const { PokerHandEvaluator } = require("../src/pokerHandEvaluator");
 
-describe('PokerHandEvaluator', () => {
-    it('should return High card: Ace', () => {
-        const playerHand = "2C 3H 4S 8C AH";
-        const pokerHandEvaluator = new PokerHandEvaluator();
+describe("PokerHandEvaluator", () => {
+  describe("High card", () => {
+    it("should return High card: Ace", () => {
+      const playerHand = "2C 3H 4S 8C AH";
+      const pokerHandEvaluator = new PokerHandEvaluator();
 
-        const result = pokerHandEvaluator.evaluate(playerHand);
+      const result = pokerHandEvaluator.evaluate(playerHand);
 
-        expect(result).toBe("high card: Ace");
+      expect(result).toBe("high card: Ace");
     });
+
+    it("should return High card: 9", () => {
+      const playerHand = "2C 3H 4S 8C 9H";
+      const pokerHandEvaluator = new PokerHandEvaluator();
+
+      const result = pokerHandEvaluator.evaluate(playerHand);
+
+      expect(result).toBe("high card: 9");
+    });
+
+    it("should return High card: Jack", () => {
+      const playerHand = "2C 3H 4S 8C JH";
+      const pokerHandEvaluator = new PokerHandEvaluator();
+
+      const result = pokerHandEvaluator.evaluate(playerHand);
+
+      expect(result).toBe("high card: Jack");
+    });
+  });
 });
