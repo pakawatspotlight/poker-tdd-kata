4b1f8d316c5eeee81b5dc9c8b7563fb94b8a407d
Pakawat Anekwiroj <pakawatanekwiroj@Pakawats-MacBook-Pro.local>
2020-04-19 00:11:59 +0700

    PokerHands - add logic compare two players

--- javascript/src/pokerHands.js
+++ javascript/src/pokerHands.js
@@ -1,14 +1,38 @@
+const { PokerHandEvaluator } = require("./pokerHandEvaluator");
+const { getCardValue, getCardText} = require("./utils");
 class PokerHands {
   constructor(black, white) {
     this._black = black;
     this._white = white;
   }
 
-  get black() { return this._black; }
-  get white() { return this._white; }
+  get black() {
+    return this._black;
+  }
+  get white() {
+    return this._white;
+  }
 
   getWinner() {
-    return "White wins. - with high card: Ace";
+    const blackScore = new PokerHandEvaluator().evaluate(this.black);
+    const whiteScore = new PokerHandEvaluator().evaluate(this.white);
+    const blackValue = getCardValue(blackScore.value);
+    const whiteValue = getCardValue(whiteScore.value);
+
+    if (blackScore.rank === whiteScore.rank) {
+      if (blackValue === whiteValue) {
+        return "Tie";
+      }
+      else if (blackValue < whiteValue) {
+        return "White wins. - with high card" + ": " + getCardText(whiteScore.value);
+      } else {
+        return "Black wins. - with high card" + ": " + getCardText(blackScore.value);
+      }
+    } else if (blackScore.rank < whiteScore.rank) {
+      return "White wins. - with flush";
+    } else {
+      return "Black wins. - with full house";
+    }
   }
 }
 exports.Pokerhands = PokerHands;
--- javascript/test/pokerHands.spec.js
+++ javascript/test/pokerHands.spec.js
@@ -23,6 +23,35 @@ describe("Poker hands", () => {
 
     expect(result).toBe("White wins. - with high card: Ace");
   });
+  it("should return black wins with high card Ace", () => {
+    const black = "2C 3H 4S 8C AH";
+    const white = "2H 3D 5S 9C KD";
+    const pokerHands = new Pokerhands(black, white);
+
+    const result = pokerHands.getWinner();
+
+    expect(result).toBe("Black wins. - with high card: Ace");
+  });
+
+  it("should return Tie", () => {
+    const black = "2H 3D 5S 9C KD";
+    const white = "2D 3H 5C 9S KH";
+    const pokerHands = new Pokerhands(black, white);
+
+    const result = pokerHands.getWinner();
+
+    expect(result).toBe("Tie");
+  });
+
+  it("should return Black wins. - with full house", () => {
+    const black = "2H 4S 4C 2D 4H";
+    const white = "2S 8S AS QS 3S";
+    const pokerHands = new Pokerhands(black, white);
+
+    const result = pokerHands.getWinner();
+
+    expect(result).toBe("Black wins. - with full house");
+  });
 });
 
 //TODO: make POkerHandEvaluator return rank of card and it value
