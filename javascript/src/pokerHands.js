2d457f5ea98f7d58968b8f6d9263e3c865682419
Pakawat Anekwiroj <pakawatanekwiroj@Pakawats-MacBook-Pro.local>
2020-04-18 20:55:05 +0700

    Add black and white hand in PokerHands class

--- javascript/src/pokerHands.js
+++ javascript/src/pokerHands.js
@@ -1,3 +1,14 @@
-exports.PokerHands = () => {
+class PokerHands {
+  constructor(black, white) {
+    this._black = black;
+    this._white = white;
+  }
 
-};
+  get black() { return this._black; }
+  get white() { return this._white; }
+
+  getWinner() {
+    return "White wins. - with high card: Ace";
+  }
+}
+exports.Pokerhands = PokerHands;
--- javascript/test/pokerHands.spec.js
+++ javascript/test/pokerHands.spec.js
@@ -1,7 +1,26 @@
-const Pokerhands = require('../src/pokerHands');
+const {Pokerhands} = require('../src/pokerHands');
 
 describe("Poker hands", () => {
-  it("should evaluate hand as a pair", () => {
-    expect(true).toBe(true);
+
+  it("should be able to set black and white player hands", () => {
+    const black = "2H 3D 5S 9C KD";
+    const white = "2C 3H 4S 8C AH";
+    const pokerHands = new Pokerhands(black, white);
+
+    const blackPlayerHand = pokerHands.black;
+    const whitePlayerHand = pokerHands.white;
+
+    expect(blackPlayerHand).toBe(black);
+    expect(whitePlayerHand).toBe(white);
+  });
+
+  it("should return white wins with high card Ace", () => {
+    const black = "2H 3D 5S 9C KD";
+    const white = "2C 3H 4S 8C AH";
+    const pokerHands = new Pokerhands(black, white);
+
+    const result = pokerHands.getWinner();
+
+    expect(result).toBe("White wins. - with high card: Ace")
   });
 });
